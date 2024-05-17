1ac2487d2cc460e6dca796f32500dd3eb3539bd4
Kevyn Rocha <kevynhohd@hotmail.com>
2024-05-16 23:01:48 -0300

    adduser adicionado e users estilizada

--- src/app/pages/home/components/users/add-user/add-user.component.ts
+++ src/app/pages/home/components/users/add-user/add-user.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-add-user',
   templateUrl: './add-user.component.html',
   styleUrls: ['./add-user.component.css']
 })
-export class AddUserComponent {
+export class AddUserComponent implements OnInit {
+  userFormGroup!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.userFormGroup = this.fb.group({
+      nome: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      funcao: ['', Validators.required],
+      senha: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(15)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.userFormGroup.valid) {
+      console.log('Novo usuário:', this.userFormGroup.value);
+    }
+  }
 }
